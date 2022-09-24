9b0c08143f8a90016ca60f87b4865fef6ca2266f
isack hernandez valenzuela <isack77@hotmail.com>
2022-09-24 12:04:48 -0700

    Agregar archivos de proyecto.

---
+++ Practica4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practica4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grados, Fahrenheit;
+
+            Console.WriteLine("Escriba los grados centigrados: ");
+            grados = Convert.ToDouble(Console.ReadLine());
+
+            Fahrenheit = (grados * 1.8000) +32 ;
+            
+            Console.WriteLine("Grados centigrados a Fahrenheit son:{0} ",Fahrenheit);
+        }
+    }
+}
